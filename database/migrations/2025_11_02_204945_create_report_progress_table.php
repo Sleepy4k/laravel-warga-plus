101a178c0a8f2af7a63130742b989b24082f92f6
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-11-02 21:48:11 +0700

    chore(database): update database migration

---
+++ database/migrations/2025_11_02_204945_create_report_progress_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Report;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('report_progress', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignIdFor(Report::class, 'report_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('report_progress');
+    }
+};
